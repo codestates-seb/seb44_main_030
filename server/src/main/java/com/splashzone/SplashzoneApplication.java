e96272c9a5035f0e0b9229ba87b7204f24098a68
devhanda <devhanda.kl@gmail.com>
2023-06-30 16:07:50 +0900

    init: 환경설정

---
+++ server/src/main/java/com/splashzone/SplashzoneApplication.java
@@ -0,0 +1,13 @@
+package com.splashzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplashzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplashzoneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/splashzone/SplashzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splashzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplashzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
