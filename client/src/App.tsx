7a359be367a65b77fe9d8ca36ee9e9a40673ceea
name <moonjonghoo@gmail.com>
2023-07-03 12:51:52 +0900

    초기세팅완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,26 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Main from "./pages/Main";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Community from "./pages/Community";
-import Club from "./pages/Club";
-import Mypage from "./pages/Mypage";
+import { Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const Main = loadable(() => import('./pages/Main'));
+const Signup = loadable(() => import('./pages/Signup'));
+const Login = loadable(() => import('./pages/Login'));
+const Community = loadable(() => import('./pages/Community'));
+const Club = loadable(() => import('./pages/Club'));
+const Mypage = loadable(() => import('./pages/Mypage'));
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Main></Main>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/community" element={<Community></Community>}></Route>
-        <Route path="/club" element={<Club></Club>}></Route>
-        <Route path="/mypage" element={<Mypage></Mypage>}></Route>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Main></Main>}></Route>
+                <Route path="/signup" element={<Signup></Signup>}></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/community" element={<Community></Community>}></Route>
+                <Route path="/club" element={<Club></Club>}></Route>
+                <Route path="/mypage" element={<Mypage></Mypage>}></Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- client/src/pages/Club.tsx
+++ client/src/pages/Club.tsx
@@ -1,5 +1,5 @@
 const Club = () => {
-  return <div>모임모집</div>;
+    return <div>모임모집</div>;
 };
 
 export default Club;
