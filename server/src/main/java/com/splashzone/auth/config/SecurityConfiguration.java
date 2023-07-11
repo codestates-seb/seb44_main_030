6a815ae72af32a41a09b783e9f436868f678d715
Seohyun-Back <lucymail100@naver.com>
2023-07-11 19:10:56 +0900

    feature: JWT 토큰 인증 코드(미완성)

--- server/src/main/java/com/splashzone/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/splashzone/auth/config/SecurityConfiguration.java
@@ -1,32 +1,51 @@
 package com.splashzone.auth.config;
 
+import com.splashzone.auth.filter.JwtAuthenticationFilter;
+import com.splashzone.auth.jwt.JwtTokenizer;
+import com.splashzone.auth.util.CustomAuthorityUtils;
+import com.splashzone.member.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 
+//@EnableWebSecurity(debug = true)
+@Configuration
 public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .headers().frameOptions().sameOrigin() // (1)
+                .headers().frameOptions().sameOrigin()
                 .and()
-                .csrf().disable()        // (2)
-                .cors(withDefaults())    // (3)
-                .formLogin().disable()   // (4)
-                .httpBasic().disable()   // (5)
+                .csrf().disable()
+                .cors(withDefaults())
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()                // (6)
+                        .anyRequest().permitAll()
                 );
         return http.build();
     }
@@ -47,5 +66,17 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+
+            builder.addFilter(jwtAuthenticationFilter);
+        }
     }
 }
